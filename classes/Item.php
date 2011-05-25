47a44cc8bb22fd95e89696110647cb1b4ea69556
Joseph Slade <js5409@doc.ic.ac.uk>
2011-05-25 15:47:20 +0100

    Added basic skeleton for file types to mirror database

---
+++ classes/Item.php
@@ -0,0 +1,8 @@
+<?php
+class Items extends DatabaseRecord {
+  public $name		= "";
+  public $value		= 0;
+  public $itemclass	= NULL;
+  public $statValue	= 0;
+  public $itemId	= -1;
+}
---
+++ classes/Player.php
@@ -0,0 +1,12 @@
+<?php
+class Player extends DatabaseRecord {
+  public $X		= 0;
+  public $Y		= 0;     	     
+  public $name		= "";
+  public $health	= 0;
+  public $wealth	= 0;
+  public $stealth	= 0;
+  public $shelf		= 0;
+  public $playerId	= -1;
+  public $shelf		= 0;
+}
