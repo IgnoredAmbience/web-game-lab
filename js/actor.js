14f2e2b0d50bba5aaa754897f28c803609e94c82
Ethel Bardsley <emb2009@ic.ac.uk>
2011-05-26 15:24:36 +0100

    Moved some stuff, and added map loading

--- js/actor.js
+++ js/actor.js
@@ -13,3 +13,20 @@ Actor.prototype.move = function (x, y) {
   this.x = x;
   this.y = y;
 }
+
+
+function Shop (x, y, color) {
+  this.x = x;
+  this.y = y;
+  this.color = color;
+}
+
+Shop.prototype.draw = function () {
+  Scene.context.fillStyle = this.color;
+  Scene.context.fillRect(this.x*10,this.y*10,10,10);
+}
+
+Shop.prototype.move = function (x, y) {
+  this.x = x;
+  this.y = y;
+}
--- js/scene.js
+++ js/scene.js
@@ -4,17 +4,20 @@ var Scene = {
     this.canvas = document.getElementById("canvas");
     this.context = canvas.getContext("2d");
     this.context.font = "bold 12px sans-serif";
+  },
 
-    this.items = new Array();
+  // For now, all the scenery contains is a list of shops. Eventually, other arguments may be passed in, which will need to be added to this.scenery by looping over :/
+  loadMap: function (shops) {
+    this.scenery = shops;
   },
 
   // TODO: Weigh up the merits of having separate item lists for scenery, actors and the player, rendered in that order. Also, scenery can probably have some nice staticy stuff done to it, maybe turned into a temporary bitmap for rendering, that's just moved around the canvas?
   draw: function () {
     toDraw = new Array();
     // for all items, if they're in view, add to toDraw
-    for (var i in this.items) {
-      if (this.inView(this.items[i])) {
-        toDraw.push(this.items[i]);
+    for (var i in this.scenery) {
+      if (this.inView(this.scenery[i])) {
+        toDraw.push(this.scenery[i]);
       }
     }
     // clear the canvas
