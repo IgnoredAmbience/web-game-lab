f4694aa4d0fef7f737e1116273309dba1e2d2261
Joseph Slade <js5409@doc.ic.ac.uk>
2011-05-26 14:44:24 +0100

    Move local-user config to tmpl files

---
+++ config.tmpl.php
@@ -0,0 +1,5 @@
+<?php
+$config['base_path'] = '/project/2010/271/g1027127(/web)?';
+$config['db'] = 'pgsql:host=db;port=5432;dbname=g1027127_u';
+$config['db_u'] = 'g1027127_u';
+$config['db_p'] = '1AP45MkDMu';
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 #!/usr/bin/php
 <?php
+require('config.php');
+
 function __autoload($name) {
   $file = "classes/$name.php";
   if(file_exists(dirname(__FILE__) . "/$file")) {
@@ -11,12 +13,8 @@ $application = new Application(array(
   array('/', 'MainHandler'),
   array('login', 'LoginHandler'),
   array('player/(\d+)', 'PlayerHandler')
-), '/project/2010/271/g1027127(/web)?');
+), $config['base_path']);
 
-$database = new PDO('pgsql:host=db;port=5432;dbname=tw1509',
-                    'tw1509',
-                    'WxZy6ewRn7',
-                    array(PDO::ATTR_PERSISTENT => true)
-                   );
+$database = new PDO($config['db'], $config['db_u'], $config['db_p'],  array(PDO::ATTR_PERSISTENT => true));
 
 $application->serve();
