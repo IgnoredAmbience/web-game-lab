c5c332ed39e08b92d252be05ac6a5ae41801cb31
Joseph Slade <js5409@doc.ic.ac.uk>
2011-05-26 17:07:13 +0100

    Added basic move function

--- classes/Player.php
+++ classes/Player.php
@@ -1,11 +1,25 @@
 <?php
 class Player extends DatabaseRecord {
-  public $X		= 0;
-  public $Y		= 0;     	     
+  public $x		= 0;
+  public $y		= 0;
   public $name		= "";
   public $health	= 0;
   public $wealth	= 0;
   public $stealth	= 0;
   public $shelf		= 0;
   public $playerId	= -1;
+
+  public function move($moveType) { //N S W E or teleport
+    //TODO add validation for tile edges; wrap or clip?
+    if($moveType == "north")
+      $this->y += 1;
+    elseif($moveType == "south")
+      $this->y -= 1;
+    elseif($moveType == "east")
+      $this->x += 1
+    elseif($moveType == "west")
+      $this->x -= 1
+    else
+      throw new Exception("unknown move type");
+  }
 }
