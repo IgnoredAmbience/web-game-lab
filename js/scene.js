bc221d01c78de0b3e2f490ced93a9a7c57a91b7a
Ethel Bardsley <emb2009@ic.ac.uk>
2011-05-31 22:07:17 +0100

    Moved that mess of IF statements into a function

--- js/scene.js
+++ js/scene.js
@@ -8,29 +8,18 @@ function loadMap (shops) {
 
 // There are separate lists for scenery, other players and the user player, rendered in that order
 function draw () {
-  // Color it grey
   toDraw = new Array();
-  // for all items, if they're in view, add to toDraw
+  // For all items, if they're in view, add to toDraw
   for (var i in scenery) {
     if (inView(scenery[i])) {
       toDraw.push(scenery[i]);
     }
   }
-  // clear the canvas
+  // Clear the canvas
   canvas.width = canvas.width;
-  // If any of the view is out of map bounds
-  context.fillStyle = "grey";
-  if (viewX < 0) context.fillRect(0,0,-viewX*TILE_SIZE,canvas.height);
-  if (maxX > mapWidth) {
-    var mapDiffX = maxX - mapWidth;
-    context.fillRect(canvas.width-(mapDiffX*TILE_SIZE),0,mapDiffX*TILE_SIZE,canvas.height);
-  }
-  if (viewY < 0) context.fillRect(0,0,canvas.width,-viewY*TILE_SIZE);
-  if (maxY > mapHeight) {
-    var mapDiffY = maxY - mapHeight;
-    context.fillRect(0,canvas.height-(mapDiffY*TILE_SIZE),canvas.width,mapDiffY*TILE_SIZE);
-  }
-  // draw them
+  // Color the edge of the map
+  colorBoundaries();
+  // draw the scenery
   for (var i in toDraw) {
     toDraw[i].draw();
   }
@@ -49,4 +38,19 @@ function inView (item) {
 // Changes the detail level
 function changeGraphics () {
   graphicsLevel = (graphicsLevel + 1) % GRAPHICS_MAX;
+}
+
+// Colors the "not map" bits of the view
+function colorBoundaries () {
+  context.fillStyle = "grey";
+  if (viewX < 0) context.fillRect(0,0,-viewX*TILE_SIZE,canvas.height);
+  if (maxX > mapWidth) {
+    var mapDiffX = maxX - mapWidth;
+    context.fillRect(canvas.width-(mapDiffX*TILE_SIZE),0,mapDiffX*TILE_SIZE,canvas.height);
+  }
+  if (viewY < 0) context.fillRect(0,0,canvas.width,-viewY*TILE_SIZE);
+  if (maxY > mapHeight) {
+    var mapDiffY = maxY - mapHeight;
+    context.fillRect(0,canvas.height-(mapDiffY*TILE_SIZE),canvas.width,mapDiffY*TILE_SIZE);
+  }
 }
