063dd0accbe06c2d68857131a31a4822d75f27e4
Joseph Slade <js5409@doc.ic.ac.uk>
2011-06-01 17:21:53 +0100

    Added some INSERTs to allow for data testing

--- createTables.sql
+++ createTables.sql
@@ -77,3 +77,32 @@ CREATE TABLE ShopStock (
   UNIQUE (shopId, itemId)
 );
 
+INSERT INTO Item (name, value, class, stat) VALUES('Spoon', 5, 'Weapon', 0);
+INSERT INTO Item (name, value, class, stat) VALUES('Fork',  6, 'Weapon', 1);
+INSERT INTO Item (name, value, class, stat) VALUES('Knife', 7, 'Weapon', 2);
+
+INSERT INTO Map (width, height, name) VALUES(64, 64, 'New Swedenheim');
+
+
+-- 3 players (alpha beta & gamma), different locations, same stats
+INSERT INTO Player (x, y, mapId, name, playing, health, wealth, stealth, shelf)
+       VALUES(5, 5, 1, 'Alpha', false, 10, 0, 0, 1);
+INSERT INTO Player (x, y, mapId, name, playing, health, wealth, stealth, shelf)
+       VALUES(6, 6, 1, 'Beta',  false, 10, 0, 0, 2);
+INSERT INTO Player (x, y, mapId, name, playing, health, wealth, stealth, shelf)
+       VALUES(7, 7, 1, 'Gamma', false, 10, 0, 0, 3);
+
+-- Alpha has a spoon, beta a fork, gamma a knife. One each
+INSERT INTO PlayerLoot (playerId, itemId, count) VALUES (1,1,1);
+INSERT INTO PlayerLoot (playerId, itemId, count) VALUES (2,2,1);
+INSERT INTO PlayerLoot (playerId, itemId, count) VALUES (3,3,1);
+
+
+INSERT INTO SHOP (x, y, mapId, name) VALUES (10, 5,  1, 'Fork Shop');
+INSERT INTO SHOP (x, y, mapId, name) VALUES (5,  10, 1, 'Spoon Shop');
+INSERT INTO SHOP (x, y, mapId, name) VALUES (10, 10, 1, 'Knife Shop');
+
+-- X shops now sell X
+INSERT INTO ShopStock (shopId, itemId, count) VALUES (1,1,5);
+INSERT INTO ShopStock (shopId, itemId, count) VALUES (2,2,5);
+INSERT INTO ShopStock (shopId, itemId, count) VALUES (3,3,5);
