9b2ea877ace3411db1e50726194d8f452978256b
Joseph Slade <js5409@doc.ic.ac.uk>
2011-06-01 15:32:06 +0100

    Added some syntax checking, error handling etc to moveHandler

--- classes/MoveHandler.php
+++ classes/MoveHandler.php
@@ -7,13 +7,18 @@ class MoveHandler extends Handler {
       <Input name="moveButton" type="submit" value="north" />
       <Input name="moveButton" type="submit" value="east" />
       <Input name="moveButton" type="submit" value="west" />
-      <Input name="moveButton" type="submit" value="S" />
+      <Input name="moveButton" type="submit" value="south" />
     </form>
   <?php
   }
 
   function post() {
-    $user = getUser();
+    $user = $this->getUser();
+    $moveType = $_POST['moveButton'];
+
+    if(!$user) {
+      throw new Exception("No User found");
+    }
 
     if(($moveType == "north") ||
        ($moveType == "south") ||
@@ -21,9 +26,9 @@ class MoveHandler extends Handler {
        ($moveType == "west" )) {
       $user->move($moveType);
     }
-    else:
-      throw new Exception("unknown move type");
-    endif;
+    else {
+      throw new Exception("unknown move type: ".$moveType);
+    }
 
     echo $_POST['moveButton'];
     echo array($user->x,$user->y);
