664c02d1381b429026730ac5dae2ad7927052dbf
Ethel Bardsley <emb2009@ic.ac.uk>
2011-06-01 15:33:09 +0100

    Should now be able to parse and list shop items

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,14 @@
 function displayShop() {
-  document.getElementById("shopDisplay").innerHTML = "SHOPPING!"
+  var sample = '[{"name":"helloooo","value":"12","itemClass":"sword","statValue":"+30"},{"name":"wibble","value":"19","itemClass":"axe","statValue":"-12"}]';
+  listItems(sample);
+}
+
+function listItems (jsonItems) {
+  var itemList = JSON.parse(jsonItems);
+  var display = document.getElementById("shopDisplay");
+  for (var i in itemList) {
+    // create list of descriptions and buy buttons
+    var item = itemList[i];
+    display.innerHTML += "<p>" + item.name + "<br>" + item.value + "<br>" + item.itemClass + "<br>" + item.statValue + "<button>"+i+"</button>" + "</p>";
+  }
 }
