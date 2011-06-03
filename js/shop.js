14ab775d1c0cf25370e520510bba4b287f215a96
Joseph Slade <js5409@doc.ic.ac.uk>
2011-06-03 17:33:12 +0100

    Shop contents should now be displayed in a slightly prettier fashion

--- js/shop.js
+++ js/shop.js
@@ -1,15 +1,28 @@
 function displayShop() {
   var sample = '[{"name":"helloooo","value":"12","itemClass":"sword","statValue":"+30"},{"name":"wibble","value":"19","itemClass":"axe","statValue":"-12"}]';
-  listItems(sample);
+  outputItems(sample);
 }
 
-function listItems (jsonItems) {
-  var itemList = JSON.parse(jsonItems);
-  var display = document.getElementById("shopDisplay");
-  display.innerHTML = "";
+function outputItems (jsonItems) {
+  document.getElementById("shopDisplay").innerHTML =
+      buildItemTable(JSON.parse(jsonItems));
+}
+
+function buildItemTable(itemList) {
+  var tableOutput = "<table border=\"1\">"
+  tableOutput += "<tr> <td> Name </td> <td> Item Class </td> <td> Stat </td> <td> Buy </td> <td> Sell </td> </tr>";
+
   for (var i in itemList) {
-    // create list of descriptions and buy buttons
-    var item = itemList[i];
-    display.innerHTML += "<p>" + item.name + "<br>" + item.value + "<br>" + item.itemClass + "<br>" + item.statValue + "<button>"+i+"</button>" + "</p>";
+    item = itemList[i];
+    tableOutput += "<tr>";
+    tableOutput += "<td>" + item.name + "</td>";
+    tableOutput += "<td>" + item.itemClass + "</td>";
+    tableOutput += "<td>" + item.statValue + "</td>";
+    tableOutput += "<td>" + "<input type=\"button\" name=\"" + item.value + "\" value=\""+item.value+"\"/> </td>";
+    tableOutput += "<td>" + "<input type=\"button\" name=\"" + item.value + "\" value=\""+item.value+"\"/> </td>";
+    tableOutput += "</tr>";
   }
+  tableOutput += "</table>";
+
+  return tableOutput;
 }
