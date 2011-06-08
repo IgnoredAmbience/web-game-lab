1c010cbfcea7e48e3c74558a63889529745ecd14
Joseph Slade <js5409@doc.ic.ac.uk>
2011-06-08 15:30:32 +0100

    Refactored some of the javascript of shops

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,4 @@
 function displayShop(id) {
-
   var httpRequest = Ajax('GET', "shop/"+id, false);
   httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   httpRequest.send(null);
@@ -13,20 +12,46 @@ function outputItems (jsonItems) {
 }
 
 function buildItemTable(itemList) {
-  var tableOutput = "<table border=\"1\">"
-  tableOutput += "<tr> <td> Name </td> <td> Item Class </td> <td> Stat </td> <td> Buy </td> <td> Sell </td> </tr>";
-
-  for (var i in itemList) {
-    item = itemList[i];
-    tableOutput += "<tr>";
-    tableOutput += "<td>" + item.name  + "</td>";
-    tableOutput += "<td>" + item.class + "</td>";
-    tableOutput += "<td>" + item.stat  + "</td>";
-    tableOutput += "<td>" + "<input type=\"button\" name=\"" + item.value + "\" value=\""+item.value+"\"/> </td>";
-    tableOutput += "<td>" + "<input type=\"button\" name=\"" + item.value + "\" value=\""+item.value+"\"/> </td>";
-    tableOutput += "</tr>";
-  }
+  var tableOutput = '<table border="1">';
+  tableOutput += ["itemID", "Name", "itemClass", "Stat", "Buy", "Sell"].map(makeTableCell).join(' ');
+  tableOutput += itemList.map(makeCellRow);
   tableOutput += "</table>";
-
   return tableOutput;
+}
+
+function makeTableCell(string) {
+  return "<td>" + string + "</td>";
+}
+
+function makeCellRow(item) {
+  var newCell = "<tr>";
+  newCell += makeTableCell(item.id);
+  newCell += makeTableCell(item.name);
+  newCell += makeTableCell(item.class);
+  newCell += makeTableCell(item.stat);
+  newCell += makeTableCell(makeShopButton(item.value, "buy"));
+  newCell += makeTableCell(makeShopButton(item.value, "sell"));
+  newCell += "</tr>";
+  return newCell;
+}
+
+function makeShopButton(itemValue, transactType, itemId) {
+    var buttonHTML = '<input type="button" name="' + itemValue + '" value ="' +
+        itemValue + '" onClick="transact(' + itemId + "," + transactType + ')" />';
+    return buttonHTML;
+}
+
+
+function transact(itemId, transactType) {
+  var httpRequest = Ajax('POST', 'login', false);
+  httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  httpRequest.send(requestString({itemId: itemId, action: transactType}));
+
+  if(httpRequest.status != 200) //200 is SUCCESS!
+      if (transactType === "buy") {
+          p.setShelf(itemId);
+      }
+      return;
+  else
+      alert("Stop fucking with the data sent to the server");
 }
