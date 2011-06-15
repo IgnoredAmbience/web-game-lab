06b32e5d3edd639969c94b8eca295aca27bd19e0
Thomas Wood <tw1509@doc.ic.ac.uk>
2011-06-15 16:16:39 +0100

    Fix the queueDump debug

--- debug/queueDump.php
+++ debug/queueDump.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
-require('classes/Notification.php');
+require('../classes/Notification.php');
 
-$q = msg_get_queue(Notification::key);
+$q = msg_get_queue(ftok('../classes/Notification.php', 'g'));
 
 while(msg_receive($q, 0, $type, 1000, $msg, true, MSG_IPC_NOWAIT, $error)) {
   $msg = json_encode($msg);
