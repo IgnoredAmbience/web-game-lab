de9db982b9b59ea83fa7bbf35604530f31c9d3c0
Joseph Slade <js5409@doc.ic.ac.uk>
2011-06-20 19:48:34 +0100

    Rewrote the lost shop code. A bug still exists with buying & selling

--- js/shop.js
+++ js/shop.js
@@ -5,8 +5,12 @@ function displayShop() {
     var httpRequest = Ajax('GET', "shop/"+id, false);
     httpRequest.send(null);
 
-    outputItems(httpRequest.responseText, "shopDisplay",
-		["Name", "Class", "Stat", "Buy", "Sell"]);
+    var shopJSON = JSON.parse(httpRequest.responseText);
+
+    var invJSON = players[Player].inventory;
+
+    outputItems(mergeStockInventoryJSON(shopJSON, invJSON), "shopDisplay",
+                ["Name", "Class", "In Shop", "On Player", "Stat", "Buy", "Sell"]);
 
     document.getElementById("shopDisplay").style.visibility = "visible"; 
   }
@@ -21,13 +25,13 @@ function outputItems (jsonItems, displayLocation, tableHeadings) {
       jsonItems = "[]";
 
   document.getElementById(displayLocation).innerHTML =
-      buildItemTable(JSON.parse(jsonItems), tableHeadings);
+      buildItemTable(jsonItems, tableHeadings);
 }
 
 function buildItemTable(itemList, tableHeadings) {
   var tableOutput = '<table>';
   tableOutput += tableHeadings.map(function(s) {return '<th>'+s+'</th>';}).join(' ');
-  tableOutput += itemList.map(makeCellRow);
+  tableOutput += itemList.map(makeCellRow).join('');
   tableOutput += "</table>";
   return tableOutput;
 }
@@ -40,20 +44,26 @@ function makeCellRow(item) {
   var newCell = "<tr>";
   newCell += makeTableCell(item.name);
   newCell += makeTableCell(item.class);
+  newCell += makeTableCell(item.shopCount);
+  newCell += makeTableCell(item.lootCount);
   newCell += makeTableCell(item.stat);
-  newCell += makeTableCell(makeShopButton(item.value, "buy", item.id));
-  newCell += makeTableCell(makeShopButton(item.value, "sell", item.id));
+  newCell += makeTableCell(makeShopButton(item, "buy"));
+  newCell += makeTableCell(makeShopButton(item, "sell"));
   newCell += "</tr>";
   return newCell;
 }
 
-function makeShopButton(itemValue, transactType, itemId) {
-  var buttonHTML = '<input type="button" name="' + itemValue + 
-      '" value ="' + itemValue + 
-      '" onClick="transact(' + itemId + ",'" + transactType + "')\"";
+function makeShopButton(item, transactType) {
+  var buttonHTML = '<input type="button" name="' + item.value +
+      '" value ="' + item.value +
+      '" onClick="transact(' + item.id + ",'" + transactType + "')\"";
 
-  if(itemValue > players[Player].wealth && transactType === "buy") {
-    buttonHTML += ' disabled="disabled" '
+  var tooExpensive = item.value > players[Player].wealth && transactType === "buy";
+  var outOfStock   = item.shopCount == 0 && transactType === "buy";
+  var notInLoot    = item.lootCount == 0 && transactType === "sell";
+
+  if (tooExpensive || outOfStock || notInLoot) {
+      buttonHTML += ' disabled="disabled" ';
   }
 
   buttonHTML += "/>";
@@ -70,4 +80,34 @@ function transact(itemId, transactType) {
     updatePlayer();
     displayShop(scenery[players[Player].x][players[Player].y].id);
   }
+}
+
+function mergeStockInventoryJSON(stock, inventory) {
+  var toReturn = stock;
+
+  for(var i = 0; i < stock.length; i++) {
+    toReturn[i].shopCount = toReturn[i].count;
+    toReturn[i].lootCount = 0;
+    delete toReturn[i].count;
+  }
+
+  for(var i = 0; i < inventory.length; i++) {
+    var inShop = false;
+    for(var j = 0; j < i && !inShop; j++) {
+      if(toReturn[j].id == inventory[i].id) {
+        toReturn[j].lootCount = inventory[i].count;
+        inShop = true
+      }
+    }
+
+    if(!inShop) {
+      var newItem = inventory[i];
+      newItem.lootCount = inventory[i].count;
+      newItem.shopCount = 0;
+      toReturn.push(newItem);
+    }
+
+  }
+
+  return toReturn;
 }
