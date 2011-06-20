659ee4f6f2090b42f5470f6d42a55c8979656d36
Thomas Wood <tw1509@doc.ic.ac.uk>
2011-06-20 16:55:22 +0100

    Drop sessions back to /tmp

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $application = new Application(array(
 $database = new PDO($config['db'], $config['db_u'], $config['db_p'],  array(PDO::ATTR_PERSISTENT => true));
 $database->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
-session_save_path('sessions');
+session_name(get_current_user());
 session_start();
 
 $application->serve();
