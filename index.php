f1f94e736b5ea5af84f2584ac8567bdde3b7a864
Thomas Wood <tw1509@doc.ic.ac.uk>
2011-06-20 21:01:42 +0100

    bah

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $application = new Application(array(
 $database = new PDO($config['db'], $config['db_u'], $config['db_p'],  array(PDO::ATTR_PERSISTENT => true));
 $database->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
-session_name(posix_getuid());
+session_name('g'.posix_getuid());
 session_start();
 
 $application->serve();
